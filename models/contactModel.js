a92fe316bc145d91c59c606a7d63a09f34d39077
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-06 21:49:55 +0300

    add mongo schema

---
+++ models/contactModel.js
@@ -0,0 +1,10 @@
+const contactSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    phoneNo: String,
+    address: String,
+    selectedImage: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+export default Contact;
