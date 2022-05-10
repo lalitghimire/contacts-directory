18961e3d5b999f59455651f0e2ec6e4dff982ecb
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-10 14:43:35 +0300

    add database connection setup

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from 'express';
 import contactRouter from './routes/contacts.js';
-const app = express();
-const PORT = 4000;
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+//express setup
+const app = express();
 app.use(express.json());
+
+const PORT = 4000;
+const mongo_uri = process.env.MONGO_URI;
+
+//connection to mongodb atlas
+mongoose
+    .connect(mongo_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('connection to database sucessful'))
+    .catch((error) => console.log('Mongodb connection fail', error.message));
+
+//routes
 app.use('/contacts', contactRouter);
 
 // listen to the server
