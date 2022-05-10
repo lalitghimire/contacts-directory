32d0da799056d2a1164c6ec5f6a50433faec3566
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-10 15:17:22 +0300

    add functionality to routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,21 +1,69 @@
 import express from 'express';
+import Contact from '../models/contactModel.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send('get all contact');
+    try {
+        const contacts = await Contact.find();
+        res.send(contacts);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
 });
+
 router.post('/', async (req, res) => {
-    res.send('post new contact');
+    const { name, email, phoneNo, address, selectedImage } = req.body;
+    let contactEntry = new Contact({ name, email, phoneNo, address, selectedImage });
+
+    try {
+        await contactEntry.save();
+        res.send(contactEntry);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
 });
+
 router.get('/:id', async (req, res) => {
-    res.send('get single contact');
+    try {
+        const contact = await Contact.findOne({ _id: req.params.id });
+
+        if (!contact) {
+            return res.send(`No contact with id ${req.params.id}`);
+        }
+        res.send(contact);
+    } catch (error) {
+        res.send(error.message);
+        console.log(error);
+    }
 });
+
 router.put('/:id', async (req, res) => {
-    res.send('update single contact');
+    try {
+        let editedContact = await Contact.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+        });
+        if (!editedContact) {
+            return res.send(`No task with id ${req.params.id}`);
+        }
+        res.send(editedContact);
+    } catch (error) {
+        res.send(error.message);
+        console.log(error);
+    }
 });
+
 router.delete('/:id', async (req, res) => {
-    res.send('delete contact');
+    try {
+        const contactDeleted = await Contact.findByIdAndDelete(req.params.id);
+        if (!contactDeleted) {
+            return res.send(`No contact with id ${req.params.id}`);
+        }
+        res.send(`contact deleted`);
+    } catch (error) {
+        res.send(error.message);
+        console.log(error);
+    }
 });
 
 export default router;
