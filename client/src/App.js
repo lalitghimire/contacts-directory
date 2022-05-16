5de396d5c98a8b3667e0b8140ac0a04ffcfbe82d
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-16 11:20:09 +0300

    add react-router and create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import AddContactForm from './components/AddContactForm';
+import { Routes, Route, Link } from 'react-router-dom';
+import ContactForm from './components/ContactForm';
 import ContactsList from './components/ContactsList';
 import Header from './components/Header';
+import { Container } from '@mui/material';
 
 const App = () => {
     return (
-        <div>
+        <Container>
             <div>
                 <Header />
             </div>{' '}
+            <Link to='/addcontact' className='btn btn-outline-dark my-5 ml-auto '>
+                Add Contact
+            </Link>{' '}
+            <Link to='/' className='btn btn-outline-dark my-5 ml-auto '>
+                Contact list
+            </Link>{' '}
+            <Link to='/editcontact' className='btn btn-outline-dark my-5 ml-auto '>
+                edit contact
+            </Link>
             <div>
-                <AddContactForm />
-                <ContactsList />
+                <Routes>
+                    <Route path='/' element={<ContactsList />} />
+                    <Route path='/addcontact' element={<ContactForm />} />
+                    <Route path='/editcontact' element={<ContactForm />} />
+                </Routes>
             </div>
-        </div>
+        </Container>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />{' '}
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 );
