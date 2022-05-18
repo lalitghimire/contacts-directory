33f4198d7b06d28241fd34de13b681968d465a3c
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-18 13:32:05 +0300

    create modal/fix updating contact

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,83 +1,120 @@
 import React from 'react';
 import { useFormik } from 'formik';
-import Button from '@mui/material/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, updateContact } from '../redux/contactSlice';
+import { Button, Dialog, DialogContent, DialogTitle, TextField } from '@mui/material';
 
-import { useDispatch } from 'react-redux';
-import { addContact } from '../redux/contactSlice';
-import { Grid } from '@mui/material';
-
-const ContactForm = () => {
+const ContactForm = ({ openModal, handleClose, currentContactId, setCurrentContactId }) => {
     const dispatch = useDispatch();
+
+    const contactTobeEdited = useSelector((state) =>
+        currentContactId
+            ? state.contactsReducer.contacts.find((c) => c.id === currentContactId)
+            : null
+    );
+
+    const initialValues =
+        currentContactId === 0
+            ? {
+                  id: '',
+                  name: '',
+                  email: '',
+                  phoneNo: '',
+                  address: '',
+                  selectedImage: '',
+              }
+            : contactTobeEdited;
     const formik = useFormik({
-        initialValues: {
-            id: '',
-            name: '',
-            email: '',
-            phoneNo: '',
-            address: '',
-            selectedImage: '',
-        },
+        initialValues,
         onSubmit: (values) => {
-            dispatch(addContact(values));
+            currentContactId === 0 ? dispatch(addContact(values)) : dispatch(updateContact(values));
             formik.resetForm();
+            handleClose();
+            setCurrentContactId(0);
         },
+        enableReinitialize: true,
     });
 
     return (
-        <div>
-            <Grid container justifyContent={'center'}>
+        <Dialog open={openModal} onClose={handleClose} aria-labelledby='form-dialog-title'>
+            <DialogTitle id='form-dialog-title'>Contact Details</DialogTitle>
+            <DialogContent>
                 <form onSubmit={formik.handleSubmit}>
-                    <div>
-                        id:{' '}
-                        <input name='id' onChange={formik.handleChange} value={formik.values.id} />
-                    </div>
-                    <div>
-                        name:{' '}
-                        <input
-                            name='name'
-                            onChange={formik.handleChange}
-                            value={formik.values.name}
-                        />
-                    </div>
-                    <div>
-                        email:{' '}
-                        <input
-                            name='email'
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                        />
-                    </div>
-                    <div>
-                        phoneNo:{' '}
-                        <input
-                            name='phoneNo'
-                            onChange={formik.handleChange}
-                            value={formik.values.phoneNo}
-                        />
-                    </div>
-                    <div>
-                        address:{' '}
-                        <input
-                            name='address'
-                            onChange={formik.handleChange}
-                            value={formik.values.address}
-                        />
-                    </div>
-                    <div>
-                        selectedImage:{' '}
-                        <input
-                            name='selectedImage'
-                            onChange={formik.handleChange}
-                            value={formik.values.selectedImage}
-                        />
-                    </div>
+                    <TextField
+                        name='id'
+                        variant='outlined'
+                        label='id'
+                        sx={{ m: 1, width: '25ch' }}
+                        style={{ width: '100%' }}
+                        InputLabelProps={{ style: { fontSize: 23 } }}
+                        onChange={formik.handleChange}
+                        value={formik.values.id}
+                    />
+
+                    <TextField
+                        name='name'
+                        variant='outlined'
+                        label='name'
+                        sx={{ m: 1, width: '25ch' }}
+                        style={{ width: '100%' }}
+                        InputLabelProps={{ style: { fontSize: 23 } }}
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                    />
+
+                    <TextField
+                        name='email'
+                        variant='outlined'
+                        label='email'
+                        sx={{ m: 1, width: '25ch' }}
+                        style={{ width: '100%' }}
+                        InputLabelProps={{ style: { fontSize: 23 } }}
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+
+                    <TextField
+                        name='phoneNo'
+                        variant='outlined'
+                        label='Phone number'
+                        sx={{ m: 1, width: '25ch' }}
+                        style={{ width: '100%' }}
+                        InputLabelProps={{ style: { fontSize: 23 } }}
+                        onChange={formik.handleChange}
+                        value={formik.values.phoneNo}
+                    />
+
+                    <TextField
+                        name='address'
+                        variant='outlined'
+                        label='Address'
+                        sx={{ m: 1, width: '25ch' }}
+                        style={{ width: '100%' }}
+                        InputLabelProps={{ style: { fontSize: 23 } }}
+                        onChange={formik.handleChange}
+                        value={formik.values.address}
+                    />
+
+                    <TextField
+                        name='selectedImage'
+                        variant='outlined'
+                        label='Image'
+                        sx={{ m: 1, width: '25ch' }}
+                        style={{ width: '100%' }}
+                        InputLabelProps={{ style: { fontSize: 23 } }}
+                        onChange={formik.handleChange}
+                        value={formik.values.selectedImage}
+                    />
 
                     <Button variant='outlined' type='submit'>
-                        Add Contact
+                        {`${currentContactId === 0 ? 'Add' : 'Update'} Contact`}
+                    </Button>
+                    <Button variant='outlined' color='secondary' onClick={handleClose}>
+                        close
                     </Button>
                 </form>
-            </Grid>
-        </div>
+            </DialogContent>
+        </Dialog>
     );
 };
 
