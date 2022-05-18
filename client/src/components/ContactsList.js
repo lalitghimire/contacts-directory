2043a43d516b2ff8780e6193fd540422eef66a2d
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-18 13:28:34 +0300

    map contacts list in tabular form

--- client/src/components/ContactsList.js
+++ client/src/components/ContactsList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {
+    Button,
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Paper,
+} from '@mui/material';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { removeContact, updateContact } from '../redux/contactSlice';
+import { removeContact } from '../redux/contactSlice';
 
-const ContactsList = () => {
+const ContactsList = ({ handleOpen, setCurrentContactId }) => {
     const contacts = useSelector((state) => state.contactsReducer.contacts);
     const dispatch = useDispatch();
     console.log('contacts', contacts);
@@ -12,29 +22,48 @@ const ContactsList = () => {
         dispatch(removeContact(id));
     };
 
-    const handleUpdateContact = () => {
-        console.log('update button clicked');
-        dispatch(
-            updateContact({
-                id: 2,
-                name: 'Lalitghimire',
-                email: 'lalit@email',
-                phoneNo: '123456',
-                address: 'Helsinki',
-                selectedImage: 'new image',
-            })
-        );
+    const handleUpdateContact = (id) => {
+        console.log('update button clicked', id);
+        setCurrentContactId(id);
+        handleOpen();
     };
     return (
         <div>
-            {contacts.map((person) => (
-                <p key={person.name}>
-                    {person.name}
-                    {person.email} {person.address}{' '}
-                    <button onClick={() => handleDeleteContact(person.id)}> delete</button>
-                    <button onClick={() => handleUpdateContact(person.id)}> edit</button>
-                </p>
-            ))}
+            <Button onClick={handleOpen}> Add Contact</Button>
+            <TableContainer sx={{ maxHeight: '300px' }} component={Paper}>
+                <Table stickyHeader aria-label='simple table'>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Phone</TableCell>
+                            <TableCell>Address</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {contacts.map((contact) => (
+                            <TableRow key={contact.id}>
+                                <TableCell> {contact.name} </TableCell>
+                                <TableCell> {contact.phoneNo} </TableCell>
+                                <TableCell> {contact.address} </TableCell>
+                                <TableCell> {contact.email} </TableCell>
+                                <TableCell>
+                                    {' '}
+                                    <button onClick={() => handleDeleteContact(contact.id)}>
+                                        {' '}
+                                        delete
+                                    </button>
+                                    <button onClick={() => handleUpdateContact(contact.id)}>
+                                        {' '}
+                                        edit
+                                    </button>{' '}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 };
