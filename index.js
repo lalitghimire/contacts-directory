8c966f0510a3ef08db4793e0739aca93fd0025a3
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-23 19:03:02 +0300

    update express middleware

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ import cors from 'cors';
 dotenv.config();
 //express setup
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded());
 
 const PORT = 4000;
 const mongo_uri = process.env.MONGO_URI;
